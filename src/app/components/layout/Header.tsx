1d59356bcd6ca7a17406465816909efbc1094c11
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-05-07 21:43:51 +0500

    update

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -8,12 +8,12 @@ import { AnimatePresence, animate, motion } from "framer-motion";
 const Header = () => {
 
   return (
-    <header className="sticky top-0 backdrop-blur-md  py-2  bg-transparent z-10 bg-opacity-90">
+    <header className="sticky top-0 backdrop-blur-md  py-2  bg-white z-10 bg-opacity-90">
 <Wrapper>
-    <div className=" mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center justify-between ">
+    <div className=" mx-auto flex  p-2  md:flex-row items-center justify-between ">
       
         {/* logo */}
-        <div className="w-[8rem]">
+        <div className="w-[5rem] md:w-[8rem]">
 
 
           <Image src={Logo} alt="Panaverse Logo" width={500} height={500} />
@@ -22,7 +22,7 @@ const Header = () => {
         </div>
         {/* Navigation bar */}
         
-        <ul className="md:ml-auto  flex flex-wrap space-x-6 font-medium text-black item-center justify-center mt-3 ">
+        <ul className=" flex  space-x-4 font-light md:space-x-6 md:font-medium text-black item-center justify-center mt-3 ">
           <li className="hover:text-purple-500">
             <Link href={'/'}>
               Home
